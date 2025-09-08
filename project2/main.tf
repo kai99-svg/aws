d27042a4b4ddb53a6d85fd1f7a94f7fa48b00053
kai99-svg <yungkai0901@gmail.com>
2025-09-09 00:24:40 +0800

    Update main.tf

--- project2/main.tf
+++ project2/main.tf
@@ -200,8 +200,8 @@ resource "aws_network_interface" "web_server2" {
 
 # EC2 Instance 1
 resource "aws_instance" "instance1" {
-  ami           = "ami-0bbdd8c17ed981ef9"
-  instance_type = "t2.micro"
+  ami           = "ami-0360c520857e3138f"
+  instance_type = "t3.micro"
   availability_zone = "us-east-1a"
   key_name = aws_key_pair.generated_key.key_name
 
@@ -225,8 +225,8 @@ resource "aws_instance" "instance1" {
 
 # EC2 Instance 2
 resource "aws_instance" "instance2" {
-  ami           = "ami-0bbdd8c17ed981ef9"
-  instance_type = "t2.micro"
+  ami           = "ami-0360c520857e3138f"
+  instance_type = "t3.micro"
   availability_zone = "us-east-1b"
   key_name = aws_key_pair.generated_key.key_name
 
